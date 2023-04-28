3b72fae87bbf67aad94835d545c799d0693a4e88
kgh0054 <kgh0054@unt.edu>
2023-04-27 20:29:20 -0500

    Added 'add' button and modal

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -1,11 +1,17 @@
 import {
-  View, Text, Pressable,
+  View, Text, Pressable, TextInput, Dimensions, Platform,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import Modal from 'react-native-modal';
+import AwesomeAlert from 'react-native-awesome-alerts';
 import { useSelector } from 'react-redux';
-import lstyles, { pawPink } from '../constants/Styles';
-import dstyles, { pawGreen } from '../constants/DarkStyles';
+import lstyles, {
+  pawPink, pawWhite,
+} from '../constants/Styles';
+import dstyles, {
+  pawYellow, pawGrey, pawGreen, pawLightGrey,
+} from '../constants/DarkStyles';
 
 export default function PetCard() {
   const [styles, setStyles] = useState(lstyles);
@@ -22,10 +28,26 @@ export default function PetCard() {
   const [walk, setWalk] = useState(false);
   const [dinner, setDinner] = useState(false);
 
+  /* toggle add goal section modal */
+  const [isAddReminderVisible, setAddReminderVisible] = useState(false);
+  const toggleAddReminder = () => {
+    setAddReminderVisible(!isAddReminderVisible);
+  };
+
+  const [reminderAdded, showReminderAdded] = useState(false);
+  const toggleAddSuccess = () => {
+    showReminderAdded(!reminderAdded);
+  };
+
+  const closeAdd = () => {
+    showReminderAdded(false);
+    setAddReminderVisible(false);
+  };
+
   return (
 
     <Pressable style={[styles.healthContainer, { paddingBottom: 20 }]}>
-      <Text style={styles.healthHeader}>Reminders</Text>
+      <Text style={styles.healthHeader}>Daily Reminders</Text>
       <View
         style={styles.healthDivider}
       />
@@ -81,6 +103,81 @@ export default function PetCard() {
             style={{ marginRight: -10 }}
           />
         </View>
+
+        <Pressable
+          style={[styles.addAppointment, { marginBottom: 0, marginTop: 15 }]}
+          onPress={toggleAddReminder}
+        >
+          <Text style={styles.appointmentButton}>
+            Add Reminder
+          </Text>
+        </Pressable>
+
+        <Modal
+          isVisible={isAddReminderVisible}
+          onBackdropPress={toggleAddReminder}
+          animationIn="fadeInUp"
+          animationInTiming={200}
+          animationOut="fadeOutDown"
+          animationOutTiming={200}
+          style={{ margin: 0 }}
+        >
+          <View style={{
+            backgroundColor: isDarkMode === 'light' ? pawLightGrey : pawWhite, padding: 30, paddingBottom: 10, borderRadius: 50,
+          }}
+          >
+            <Pressable style={[styles.menuItem, { width: Dimensions.get('window').width - 40, backgroundColor: isDarkMode === 'light' ? pawYellow : pawGreen }]}>
+              <Text
+                style={[styles.menuText, styles.accountFields, { color: isDarkMode === 'light' ? pawGrey : pawWhite }]}
+              >
+                Reminder Name
+              </Text>
+              <TextInput
+                autoCorrect={false}
+                clearTextOnFocus
+                autoCapitalize="words"
+                placeholder="Name"
+                placeholderTextColor={isDarkMode === 'light' ? pawGrey : pawGrey}
+                style={[styles.menuText, { fontSize: 22, width: 'auto', marginRight: Platform.OS === 'android' ? 20 : 0 }]}
+              />
+            </Pressable>
+
+            <Pressable
+              style={[styles.submitbutton, { width: Dimensions.get('window').width - 40 }]}
+              onPress={toggleAddSuccess}
+            >
+              <Text
+                style={styles.submittext}
+              >
+                Add Reminder
+              </Text>
+            </Pressable>
+
+            <AwesomeAlert
+              show={reminderAdded}
+              title="Goal Added!"
+              confirmText="Yay!"
+              titleStyle={styles.alertText}
+              contentContainerStyle={styles.alertBackground}
+              showConfirmButton
+              confirmButtonTextStyle={styles.confirmButton}
+              onConfirmPressed={closeAdd}
+              style={{ borderRadius: 50, overflow: 'hidden' }}
+              confirmButtonColor={isDarkMode === 'light' ? pawGreen : pawPink}
+            />
+
+            <Pressable
+              style={[styles.submitbutton, { width: Dimensions.get('window').width - 40, backgroundColor: isDarkMode === 'light' ? '#d94545' : '#b81d1d' }]}
+              onPress={toggleAddReminder}
+            >
+              <Text
+                style={styles.submittext}
+              >
+                Cancel
+              </Text>
+            </Pressable>
+          </View>
+        </Modal>
 
       </View>
     </Pressable>
